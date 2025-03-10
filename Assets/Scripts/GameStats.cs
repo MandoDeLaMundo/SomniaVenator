acd7a9b9d28bc13e401a6e60a9d6a87f311f179a
Montiel_Armando <armtiel12@gmail.com>
2025-03-10 03:12:34 -0400

    ADDED Score scripts

--- Assets/Scripts/GameStats.cs
+++ Assets/Scripts/GameStats.cs
@@ -7,17 +7,20 @@ public class GameStats : MonoBehaviour
 {
     [SerializeField] private Stopwatch stopwatchScript;
 
-    public int starsCollected;
     public TextMeshProUGUI starsCollectedText;
     public TextMeshProUGUI timeLapseText;
 
     // Update is called once per frame
     void Update()
     {
-        starsCollectedText.text = "Stars collected: " + starsCollected.ToString();
 
-        TimeSpan timeSpan = TimeSpan.FromSeconds(stopwatchScript.currentTime);
-        TimeSpan time = timeSpan;
-        timeLapseText.text = "Time: " + time.ToString(@"mm\:ss\:fff");
+
+        if (stopwatchScript != null && timeLapseText != null)
+        {
+            TimeSpan timeSpan = TimeSpan.FromSeconds(stopwatchScript.currentTime);
+            TimeSpan time = timeSpan;
+            timeLapseText.text = "Time: " + time.ToString(@"mm\:ss\:fff");
+        }
+
     }
 }
