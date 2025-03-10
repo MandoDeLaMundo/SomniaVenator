27e97e3daf9b387716048a08884966d385d93802
Darkeye7778 <finnagainlarose@gmail.com>
2025-03-10 01:59:22 -0400

    FIXED Steps and Jump audio
    Maybe??

--- Assets/Scripts/SoundScript.cs
+++ Assets/Scripts/SoundScript.cs
@@ -1,11 +1,42 @@
 using UnityEngine;
 using System;
+using UnityEngine.InputSystem;
 
 public class SoundScript : MonoBehaviour
 {
-    public AudioSource soundSource;
-    public AudioClip footsteps;
+    public AudioSource mWalkSource;
+    //public AudioClip footsteps;
+    public AudioSource mJumpSource;
+    //[SerializeField] private PlayerMovement moveSound;
+    public void Move(InputAction.CallbackContext context)
+    {
+        if (context.performed && !mWalkSource.isPlaying)
+        {
+            mWalkSource.Play();
+        }
+        else if (context.performed && mWalkSource.isPlaying)
+        {
 
-    [SerializeField] private PlayerMovement moveSound;
+        }
+        else
+        {
+            mWalkSource.Stop();
+        }
+    }
 
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && !mJumpSource.isPlaying)
+        {
+            mJumpSource.Play();
+        }
+        else if (context.performed && mJumpSource.isPlaying)
+        {
+
+        }
+        else
+        {
+            mJumpSource.Stop();
+        }
+    }
 }
